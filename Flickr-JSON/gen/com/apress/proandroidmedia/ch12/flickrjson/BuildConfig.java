d73f3379383ee1c7de6bf5bb49f9b6f6feb51cd2
danhnguyen <danhnguyen0902@gmail.com>
2014-02-25 15:35:31 -0500

    Flickr Authentication

---
+++ Flickr-JSON/gen/com/apress/proandroidmedia/ch12/flickrjson/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.apress.proandroidmedia.ch12.flickrjson;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Location-Flickr-JSON/gen/com/apress/proandroidmedia/ch12/flickrjsonlocation/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.apress.proandroidmedia.ch12.flickrjsonlocation;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
